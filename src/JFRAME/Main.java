532f3cab97f5f67a7ed60f3dd378d1c98862a02f
SOKONG SOK <100309426+Jeff-kxng@users.noreply.github.com>
2023-04-24 22:18:00 +0700

    Initial commit

---
+++ src/JFRAME/Main.java
@@ -0,0 +1,21 @@
+package JFRAME;
+import javax.swing.ImageIcon;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // JFrame = a GUI window to add component to
+      /*  JFrame frame = new JFrame(); // Create a frame
+        frame.setTitle("Jframe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(420, 420); // sets the x-dimension y- dimension
+        frame.setVisible(true); // make frame visible
+        ImageIcon imageIcon = new ImageIcon("iava.png");
+        frame.setIconImage(imageIcon.getImage());
+        frame.getContentPane().setBackground(Color.CYAN); // Change color of background
+       */
+        
+    }
+}
---
+++ src/JFRAME/MyFrame.java
@@ -0,0 +1,20 @@
+package JFRAME;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame(){
+        /* this.setTitle("Jthis");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(420, 420); // sets the x-dimension y- dimension
+        this.setVisible(true); // make this visible
+        ImageIcon imageIcon = new ImageIcon("iava.png");
+        this.setIconImage(imageIcon.getImage());
+        this.getContentPane().setBackground(Color.CYAN);
+         */
+        MyFrame myFrame = new MyFrame();
+    }
+
+}
