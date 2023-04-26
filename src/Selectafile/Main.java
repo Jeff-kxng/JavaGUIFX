c410e01678c1c61192a2318283d3a235908183e5
SOKONG SOK <100309426+Jeff-kxng@users.noreply.github.com>
2023-04-26 14:01:26 +0700

    JFileselected

---
+++ src/Selectafile/Main.java
@@ -0,0 +1,8 @@
+package Selectafile;
+
+public class Main {
+    public static void main(String[] args) {
+        // JFilesected = a gui mechanism that let's a user choose a file (helpful for open or saving files)
+    new MyJframe();
+    }
+}
